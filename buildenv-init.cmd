6f787974223f3de4535ec6e9eac5e588cd23e5d7
Salty <mark@turbo.net>
2021-02-19 14:42:07 -0800

    add script to start build environment

---
+++ buildenv-init.cmd
@@ -0,0 +1,7 @@
+
+mkdir %USERPROFILE%\.m2
+copy %~dp0\maven-settings.xml %USERPROFILE%\.m2\settings.xml 
+
+title Jenkins Build Env
+
+cls
---
+++ buildenv.cmd
@@ -0,0 +1,2 @@
+
+turbo try jdk:8,maven:3,git --mount=%~dp0 -d --startup-file=cmd -- /k %~dp0\buildenv-init.cmd
